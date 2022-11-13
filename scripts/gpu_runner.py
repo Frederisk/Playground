53e52145692e02ee2d71e8691646fe07fb2f65ea
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2022-11-13 20:23:30 +0800

    Update gpu_runner.py

--- scripts/gpu_runner.py
+++ scripts/gpu_runner.py
@@ -2,5 +2,10 @@ import os
 
 print('Hello World!')
 
-os.system('command -V docker')
+status = os.system('command -V docker')
+
+if status:
+    raise Exception('docker NOT FOUND!')
+
+raise Exception('Force Failed!')
 # os.system('command -V ')
